378411cf0bb0671c182d6e042a948c3059f78560
Matt DeGoey <degoeym@gmail.com>
2015-01-10 00:22:48 -0500

    Latest changes

---
+++ OdeToFood/Controllers/CuisineController.cs
@@ -0,0 +1,25 @@
+﻿using OdeToFood.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class CuisineController : Controller
+    {
+        //
+        // GET: /Cuisine/
+        [Log]
+        public ActionResult Search(string name = "french")
+        {
+            throw new Exception("Something terrible has happened");
+            
+            var message = Server.HtmlEncode(name);
+
+            return Content(message);
+        }
+
+    }
+}
