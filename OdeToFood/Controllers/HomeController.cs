d4383850ec71e3f9d879e2e5e8f99f85566af8eb
Matt DeGoey <degoeym@gmail.com>
2015-01-27 21:58:19 -0500

    Learning ASP.NET MVC 4, Working With Data Part I

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -9,17 +9,25 @@ namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        OdeToFoodDb _db = new OdeToFoodDb();
+
+        public ActionResult Index(string searchTerm = null)
         {
-            var controller = RouteData.Values["controller"];
-            var action = RouteData.Values["action"];
-            var id = RouteData.Values["id"];
+            var model =
+                _db.Restaurants
+                    .OrderByDescending(r => r.Reviews.Average(review => review.Rating))
+                    .Where(r => searchTerm == null || r.Name.StartsWith(searchTerm))
+                    .Take(10)
+                    .Select(r => new RestaurantListViewModel
+                        {
+                            Id = r.Id,
+                            Name = r.Name,
+                            City = r.City,
+                            Country = r.Country,
+                            CountOfReviews = r.Reviews.Count()
+                        });
 
-            var message = String.Format("{0}::{1} {2}", controller, action, id);
-
-            ViewBag.Message = message;
-
-            return View();
+            return View(model);
         }
 
         public ActionResult About()
@@ -36,6 +44,15 @@ namespace OdeToFood.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_db != null)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
